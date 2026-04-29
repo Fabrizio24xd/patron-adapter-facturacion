e0d038aaa8f9b6012dec76a9ccc7b98fc5a491ba
Fabrizio24xd <149338660+Fabrizio24xd@users.noreply.github.com>
2026-04-28 20:29:40 -0500

    Add LegacyBillingSystem class for old invoice format

---
+++ LegacyBillingSystem.java
@@ -0,0 +1,6 @@
+public class LegacyBillingSystem {
+    public void generate_invoice_old_format(String data) {
+        System.out.println("[Legacy System] Procesando en sistema antiguo...");
+        System.out.println("[Legacy System] Datos recibidos: " + data);
+    }
+}
